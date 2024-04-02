7581ca199ad06e5a82fe120244d02ddf4fca69e5
Ed <silva.email@yahoo.com>
2024-04-02 12:09:57 -0500

    Add item functionality and clear input field after pushing the item

--- main.js
+++ main.js
@@ -2,6 +2,17 @@ Vue.createApp({
   data() {
     return {
       item: "",
+      listOfItems: []
     };
   },
+  methods: {
+      
+    testButton() {
+            console.log('Hello');      
+      this.listOfItems.push(this.item);
+    
+      this.item = ""; 
+      // Clear the input field after pushing the item
+    },
+  }
 }).mount("#app");
